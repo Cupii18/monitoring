cda0c2826b228409928f49be4cfa9e9947b87928
Cupii18 <100671535+Cupii18@users.noreply.github.com>
2022-08-15 13:29:15 +0700

    Delete API directory

--- API/router/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/petugas/" ,require("../petugas/petugas"));
-router.use("/monitoring", require("../monitoring/monitoring"));
-
-
-module.exports = router;
