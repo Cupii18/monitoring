e5068665882c8182009f7cc22b886e712b30232d
Cupii18 <ekamayanauranabila@gmail.com>
2022-09-15 17:39:43 +0700

    bbb

--- router/index.js
+++ router/index.js
@@ -1,14 +1,5 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/admin" ,require("../admin/admin"));
-router.use("/report", require("../report/report"));
-router.use("/petugas", require("../petugas/petugas"));
-router.use("/monitor", require("../monitor/monitor"));
-router.use("/alert", require("../alert/alert"));
-router.use("/histori", require("../histori/histori"));
-router.use("/kategori", require("../kategori/kategori"));
-router.use("/dashboard", require("../dashboard/dashboard"));
-
 
 module.exports = router;
