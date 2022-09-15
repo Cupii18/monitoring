1b6ca109bf50451f93d34c6f9971c4a02e55de37
Cupii18 <ekamayanauranabila@gmail.com>
2022-09-15 17:21:21 +0700

    jjjj

--- router/index.js
+++ router/index.js
@@ -6,6 +6,9 @@ router.use("/report", require("../report/report"));
 router.use("/petugas", require("../petugas/petugas"));
 router.use("/monitor", require("../monitor/monitor"));
 router.use("/alert", require("../alert/alert"));
+router.use("/histori", require("../histori/histori"));
+router.use("/kategori", require("../kategori/kategori"));
+router.use("/dashboard", require("../dashboard/dashboard"));
 
 
 module.exports = router;
