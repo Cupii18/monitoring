ce39ca91c791dbb063adc4785634455a796bc31e
cupii18 <sity.sofiani@gmail.com>
2022-09-21 19:09:55 +0700

    monitor

---
+++ router/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.use("/petugas/" ,require("../petugas/petugas"));
+router.use("/jabatan", require("../jabatan/jabatan"));
+router.use("/jenis_device", require("../jenis_device/jenis_device"));
+router.use("/sektor", require("../sektor/sektor"));
+router.use("/indikator", require("../indikator/indikator"));
+router.use("/device", require("../device/device"));
+router.use("/monitor_dc", require("../monitor_dc/monitor_dc"));
+router.use("/alert", require("../alert/alert"));
+
+
+
+
+module.exports = router;
