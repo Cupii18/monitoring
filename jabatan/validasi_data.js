66016a5ffc41fed556d0cdd8084582f11f654cfe
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-10-24 00:42:36 +0700

    Update Response, Handilng CRUD

--- jabatan/validasi_data.js
+++ jabatan/validasi_data.js
@@ -1,12 +1,10 @@
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 
 module.exports.data = [
     check('nama_jabatan').not().isEmpty().withMessage('nama_jabatan harus ada data'),
-    check('status').not().isEmpty().withMessage('status harus ada data')
-    
+
 ]
 
 module.exports.edit_data = [
     check('nama_jabatan').not().isEmpty().withMessage('nama_jabatan harus ada data'),
-    check('status').not().isEmpty().withMessage('status harus ada data')
 ]
