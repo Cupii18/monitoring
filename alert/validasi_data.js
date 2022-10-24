e40983a088bf2d1150b99cb5e48d52a62b8ac180
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-10-25 00:41:12 +0700

    Update validasi_data.js

--- alert/validasi_data.js
+++ alert/validasi_data.js
@@ -1,13 +1,12 @@
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 
 module.exports.data = [
-    check('id_petugas').not().isEmpty().withMessage('id_petugas harus ada data'),
-    check('id_device').not().isEmpty().withMessage('id_device harus ada data'),
-    check('nama_alert').not().isEmpty().withMessage('nama_alert harus ada data'),
-    check('kondisi').not().not().isEmpty().withMessage('kondisi  harus ada data'),
-    check('interval').not().not().isEmpty().withMessage('interval harus ada data'),
-    check('status').not().not().isEmpty().withMessage('status harus ada data'),
-    check('tanggal').not().not().isEmpty().withMessage('tangggal harus ada data')
+    check('id_petugas').not().isEmpty().withMessage('Petugas tidak boleh kosong'),
+    check('id_device').not().isEmpty().withMessage('Device tidak boleh kosong'),
+    check('nama_alert').not().isEmpty().withMessage('Nama Alert tidak boleh kosong'),
+    check('kondisi').not().not().isEmpty().withMessage('Kondisi tidak boleh kosong'),
+    check('interval').not().not().isEmpty().withMessage('Interval tidak boleh kosong'),
+    check('tanggal').not().not().isEmpty().withMessage('Tanggal tidak boleh kosong'),
 
 ]
 
@@ -17,6 +16,5 @@ module.exports.edit_data = [
     check('nama_alert').not().isEmpty().withMessage('nama_alert harus ada data'),
     check('kondisi').not().not().isEmpty().withMessage('kondisi  harus ada data'),
     check('interval').not().not().isEmpty().withMessage('interval harus ada data'),
-    check('status').not().not().isEmpty().withMessage('status harus ada data'),
     check('tanggal').not().not().isEmpty().withMessage('tangggal harus ada data')
 ]
