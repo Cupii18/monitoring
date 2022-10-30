ab0756c0f8b52bbfdc3fa169e138f4049f77b7d1
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-10-30 20:48:35 +0700

    Update monitor_dc.js

--- monitor_dc/monitor_dc.js
+++ monitor_dc/monitor_dc.js
@@ -5,24 +5,24 @@ const database = require("../config/database");
 router.get('/', async (req, res) => {
     try {
         const result = await database
-            .select('tb_device.nama_device', 'monitor_dc.waktu', 'monitor_dc.tegangan', 'monitor_dc.arus', 'monitor_dc.whatt', 'monitor_dc.kwh', 'tb_indikator.nama_indikator', 'tb_indikator.satuan')
+            .select(
+                'monitor_dc.id_monitor_dc',
+                'tb_device.nama_device',
+                'tb_jenis_device.nama_jenis',
+                'monitor_dc.waktu',
+                'monitor_dc.tegangan',
+                'monitor_dc.arus',
+                'monitor_dc.watt',
+                'monitor_dc.kwh',
+                'tb_indikator.nama_indikator',
+                'tb_indikator.satuan'
+            )
             .from('monitor_dc')
             .join('tb_device', 'monitor_dc.id_device', 'tb_device.id_device')
+            .join('tb_jenis_device', 'tb_device.id_jenis_device', 'tb_jenis_device.id_jenis_device')
             .join('tb_indikator', 'tb_device.id_device', 'tb_indikator.id_device')
             .join('tb_sektor', 'tb_device.id_sektor', 'tb_sektor.id_sektor')
-            .modify(function (queryBuilder) {
-                if (req.query.id_sektor) {
-                    queryBuilder.where('tb_sektor.id_sektor', req.query.id_sektor)
-                }
-
-                if (req.query.id_device) {
-                    queryBuilder.where('tb_device.id_device', req.query.id_device)
-                }
-
-                if (req.query.id_indikator) {
-                    queryBuilder.where('tb_indikator.id_indikator', req.query.id_indikator)
-                }
-            })
+            .groupBy('monitor_dc.id_device', 'tb_indikator.id_indikator', 'tb_jenis_device.id_jenis_device')
 
         if (result) {
             return res.status(200).json({
@@ -47,46 +47,54 @@ router.get('/', async (req, res) => {
 // Post
 router.post('/', async (req, res) => {
     try {
-        // Split string
-        const sensor = req.query.sensor.split(", ") || req.body.sensor.split(", ");
-        const checkDevice = await database("tb_device")
-            .join("tb_jenis_device", "tb_device.id_jenis_device", "tb_jenis_device.id_jenis_device")
-            .join("tb_sektor", "tb_device.id_sektor", "tb_sektor.id_sektor")
-            .where("tb_device.nama_device", sensor[0])
-            .where("tb_jenis_device.nama_jenis", sensor[1])
-            .where("tb_sektor.nama_sektor", req.query.lokasi ? req.query.lokasi : req.body.lokasi)
-            .select("tb_device.id_device")
-            .first();
+        // contains ','
+        if (req.body.sensor.includes(', ')) {
+            const sensor = req.body.sensor.split(", ");
 
-        if (!checkDevice) {
-            return res.status(400).json({
-                status: 0,
-                message: "Device tidak ditemukan"
-            });
-        }
+            const checkDevice = await database("tb_device")
+                .join("tb_jenis_device", "tb_device.id_jenis_device", "tb_jenis_device.id_jenis_device")
+                .join("tb_sektor", "tb_device.id_sektor", "tb_sektor.id_sektor")
+                .where("tb_device.nama_device", sensor[0])
+                .where("tb_jenis_device.nama_jenis", sensor[1])
+                .where("tb_sektor.nama_sektor", req.body.lokasi)
+                .select("tb_device.id_device")
+                .first();
 
-        const data = {
-            id_device: checkDevice.id_device,
-            tegangan: parseFloat(req.query.tegangan ? req.query.tegangan : req.body.tegangan),
-            arus: parseFloat(req.query.arus ? req.query.arus : req.body.arus),
-            watt: parseFloat(req.query.watt ? req.query.watt : req.body.watt),
-            kwh: parseFloat(req.query.kwh ? req.query.kwh : req.body.kwh),
-            status: req.query.status ? req.query.status : req.body.status,
-            waktu: new Date(),
-            created_at: new Date(),
-            updated_at: new Date()
-        }
+            if (!checkDevice) {
+                return res.status(400).json({
+                    status: 0,
+                    message: "Device tidak ditemukan"
+                });
+            }
 
-        const insert = await database("monitor_dc").insert(data);
-        if (insert) {
-            return res.status(201).json({
-                status: 1,
-                message: "Berhasil"
-            })
+            const data = {
+                id_device: checkDevice.id_device,
+                tegangan: parseFloat(req.body.tegangan),
+                arus: parseFloat(req.body.arus),
+                watt: parseFloat(req.body.watt),
+                kwh: parseFloat(req.body.kwh),
+                status: req.body.status,
+                waktu: new Date(),
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+
+            const insert = await database("monitor_dc").insert(data);
+            if (insert) {
+                return res.status(201).json({
+                    status: 1,
+                    message: "Berhasil"
+                })
+            } else {
+                return res.status(422).json({
+                    status: 0,
+                    message: "Gagal"
+                })
+            }
         } else {
             return res.status(422).json({
                 status: 0,
-                message: "Gagal"
+                message: "Nama dan jenis device tidak dapat ditemukan"
             })
         }
     } catch (error) {
